fe7db69a23c669b52b6b3660dcb50587872205cc
Pedro Uchoa <joaopedrouchoacampos@gmail.com>
2025-07-06 15:32:17 -0300

    First Commit

---
+++ src/main/java/com/example/chat_spring/ChatSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.chat_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSpringApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
